1ca802f26725a55ea3828b9d7fd90618558eadd7
Seth Lachman <lachmanseth17@gmail.com>
2019-06-14 11:34:13 -0400

    Added answers to readme and started working on server.

--- README.md
+++ README.md
@@ -29,9 +29,20 @@ Implement an User Authentication System in order to access the jokes from the Jo
 Demonstrate your understanding of this week's concepts by answering the following free-form questions. Edit this document to include your answers after each question. Make sure to leave a blank line above and below your answer so it is clear and easy to read by your project manager.
 
 1. What is the purpose of using _sessions_?
+
+Sessions allow the server to store information about the client so that a user doesn’t need to log in whenever they make a new request.
+
 1. What does bcrypt do to help us store passwords in a secure manner.
+
+Bcrypt hashes the given password so that if the application is breached, the attacker will not be able to easily derive the password from the hash.
+
 1. What does bcrypt do to slow down attackers?
+
+Bcrypt hashes the input received multiple times to increase the time needed for an attacker to break the hash.
+
 1. What are the three parts of the JSON Web Token?
+
+Three parts of a JSON Web Token: header (algorithm type and token type), payload (contains information such as the subject and initialized time of the token), and verify signature of the token.
 
 ## Project Set Up
 
@@ -48,9 +59,9 @@ Follow these steps for completing your project:
 
 - [ ] `cd` into the root of the project and run `yarn` to install dependencies.
 - [ ] Once you have your `node_modules` go ahead and run `yarn server` or `npm run server` to start your node server.
-- [ ] Submit a Pull-Request to merge <firstName-lastName> Branch into master (student's  Repo).
+- [ ] Submit a Pull-Request to merge <firstName-lastName> Branch into master (student's Repo).
 - [ ] Add your Project Manager as a Reviewer on the Pull-request
-- [ ] PM then will count the HW as done by  merging the branch back into master.
+- [ ] PM then will count the HW as done by merging the branch back into master.
 
 Helpful Tip on Testing this Project:
 
--- config/routes.js
+++ config/routes.js
@@ -1,32 +1,76 @@
-const axios = require('axios');
+const axios = require("axios");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-const { authenticate } = require('../auth/authenticate');
+const { authenticate } = require("../auth/authenticate");
+const Users = require("./user-model.js");
 
 module.exports = server => {
-  server.post('/api/register', register);
-  server.post('/api/login', login);
-  server.get('/api/jokes', authenticate, getJokes);
+  server.post("/api/register", register);
+  server.post("/api/login", login);
+  server.get("/api/jokes", authenticate, getJokes);
 };
 
 function register(req, res) {
-  // implement user registration
+  const user = req.body;
+  const hash = bcrypt.hashSync(user.password, 8);
+  user.password = hash;
+
+  Users.add(user)
+    .then(savedUser => {
+      res.status(201).json(savedUser);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 }
 
 function login(req, res) {
-  // implement user login
+  const { username, password } = req.body;
+  Users.findUser({ username })
+    .first()
+    .then(user => {
+      console.log(user);
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+
+        res.status(200).json({
+          message: `Welcome ${user.username}`,
+          token
+        });
+      } else {
+        res.status(401).json({ message: "You shall not pass!" });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 }
 
 function getJokes(req, res) {
   const requestOptions = {
-    headers: { accept: 'application/json' },
+    headers: { accept: "application/json" }
   };
 
   axios
-    .get('https://icanhazdadjoke.com/search', requestOptions)
+    .get("https://icanhazdadjoke.com/search", requestOptions)
     .then(response => {
       res.status(200).json(response.data.results);
     })
     .catch(err => {
-      res.status(500).json({ message: 'Error Fetching Jokes', error: err });
+      res.status(500).json({ message: "Error Fetching Jokes", error: err });
     });
+}
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id, // standard claim = sub
+    username: user.username // additional data
+  };
+
+  const options = {
+    expiresIn: "8h"
+  };
+
+  return jwt.sign(payload, secrets.jwtSecret, options);
 }
